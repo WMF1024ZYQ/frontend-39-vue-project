8e3f113b7b73c86bb237050e56b57def6fed89fb
wmf <q8691358412@hotmail.com>
2019-06-27 22:41:54 +0800

    登录页面

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
+import "./assets/css/index.css";
 
 Vue.config.productionTip = false;
+Vue.use(ElementUI);
 
+router.beforeEach((to, from, next) => {
+  if (to.path === "/login") {
+    next();
+    return;
+  }
+
+  if (!localStorage.getItem("token")) {
+    router.push("/login");
+  }
+  next();
+});
 new Vue({
   router,
   render: h => h(App)
